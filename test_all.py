0a49543ccaf68e37f2a99b87524bbe3d89ded8dd
David Zemon <david.zemon@redlion.net>
2022-02-07 22:51:09 -0600

    Add requirements file; Switch test_all script to Python

---
+++ test_all.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import subprocess
+
+test_cases = [
+    ['--no-library'],
+    ['--no-library', '--no-service'],
+    ['--no-executable'],
+    ['--no-tests', '--no-library'],
+    ['--no-tests', '--no-library', '--no-service'],
+    ['--no-tests', '--no-executable'],
+    ['--namespace=Zemon', '--no-library'],
+    ['--namespace=Zemon', '--no-library', '--no-service'],
+    ['--namespace=Zemon', '--no-executable'],
+    ['--namespace=Zemon', '--no-tests', '--no-library'],
+    ['--namespace=Zemon', '--no-tests', '--no-library', '--no-service'],
+    ['--namespace=Zemon', '--no-tests', '--no-executable'],
+    ['--no-cxx'],
+    ['--no-cxx', '--no-library'],
+    ['--no-cxx', '--no-library', '--no-service'],
+    ['--no-cxx', '--no-executable'],
+    ['--no-cxx', '--no-tests'],
+    ['--no-cxx', '--no-tests', '--no-library'],
+    ['--no-cxx', '--no-tests', '--no-library', '--no-service'],
+    ['--no-cxx', '--no-tests', '--no-executable']
+]
+
+for case in test_cases:
+    subprocess.run(['bash', 'test.sh'] + case, check=True)
