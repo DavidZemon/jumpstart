a0b5ee4992d05bfa1ca79333931bbb7e2f483c2f
David Zemon <david.zemon@redlion.net>
2022-02-08 16:50:26 -0600

    Use Python's unit testing framework

--- test_all.py
+++ test_all.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
 
+import os
+import shutil
 import subprocess
-from test import test
+import sys
+import unittest
+from parameterized import parameterized
+from typing import List
 
-test_cases = [
-    ['--no-library'],
-    ['--no-library', '--no-service'],
-    ['--no-executable'],
-    ['--no-tests', '--no-library'],
-    ['--no-tests', '--no-library', '--no-service'],
-    ['--no-tests', '--no-executable'],
-    ['--namespace=Zemon', '--no-library'],
-    ['--namespace=Zemon', '--no-library', '--no-service'],
-    ['--namespace=Zemon', '--no-executable'],
-    ['--namespace=Zemon', '--no-tests', '--no-library'],
-    ['--namespace=Zemon', '--no-tests', '--no-library', '--no-service'],
-    ['--namespace=Zemon', '--no-tests', '--no-executable'],
-    ['--no-cxx'],
-    ['--no-cxx', '--no-library'],
-    ['--no-cxx', '--no-library', '--no-service'],
-    ['--no-cxx', '--no-executable'],
-    ['--no-cxx', '--no-tests'],
-    ['--no-cxx', '--no-tests', '--no-library'],
-    ['--no-cxx', '--no-tests', '--no-library', '--no-service'],
-    ['--no-cxx', '--no-tests', '--no-executable']
-]
 
-for case in test_cases:
-    test(case)
+class JumpstartTest(unittest.TestCase):
+    def setUp(self):
+        if os.path.exists('generated'):
+            shutil.rmtree('generated')
+
+    @parameterized.expand([
+        [['--no-library']],
+        [['--no-library', '--no-service']],
+        [['--no-executable']],
+        [['--no-tests', '--no-library']],
+        [['--no-tests', '--no-library', '--no-service']],
+        [['--no-tests', '--no-executable']],
+        [['--namespace=Zemon', '--no-library']],
+        [['--namespace=Zemon', '--no-library', '--no-service']],
+        [['--namespace=Zemon', '--no-executable']],
+        [['--namespace=Zemon', '--no-tests', '--no-library']],
+        [['--namespace=Zemon', '--no-tests', '--no-library', '--no-service']],
+        [['--namespace=Zemon', '--no-tests', '--no-executable']],
+        [['--no-cxx']],
+        [['--no-cxx', '--no-library']],
+        [['--no-cxx', '--no-library', '--no-service']],
+        [['--no-cxx', '--no-executable']],
+        [['--no-cxx', '--no-tests']],
+        [['--no-cxx', '--no-tests', '--no-library']],
+        [['--no-cxx', '--no-tests', '--no-library', '--no-service']],
+        [['--no-cxx', '--no-tests', '--no-executable']]
+    ])
+    def test_all(self, args: List[str]):
+        subprocess.run([sys.executable, './jumpstart.py', '--defaults'] + args, check=True)
+        subprocess.run(['conan', 'install', '-if', 'generated/build', 'generated'], check=True)
+        subprocess.run(['conan', 'build', '-bf', 'generated/build', 'generated'], check=True)
+        subprocess.run(['make', '-C', 'generated/build', 'docs', 'package', '-j', '30'], check=True)
